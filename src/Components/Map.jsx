38377c11982d10ef5f634a8dffc49f25dbdca2bd
Oktay Rasimov <oktayrasimov123@gmail.com>
2023-09-29 15:46:43 +0300

    added geolocation api and installed react query

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -9,7 +9,7 @@ import {
   useMapEvents,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { useGetUrlPosition } from "../Data/useGetUrlPosition";
 
@@ -21,13 +21,32 @@ const MapContainerDiv = styled(MapContainer)`
 
 function MapClick() {
   const navigate = useNavigate();
-  useMapEvent({
-    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  const map = useMapEvent({
+    click: (e) => {
+      console.log(e);
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+      map.setView(e.latlng);
+    },
   });
 }
 
 function Map() {
-  const position = useGetUrlPosition();
+  const [lat, lng] = useGetUrlPosition();
+
+  useEffect(
+    function () {
+      async function getCity() {
+        const res = await fetch(
+          `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+        );
+        const data = await res.json();
+        console.log(data);
+      }
+      getCity();
+    },
+    [lat, lng]
+  );
+
   return (
     <MapContainerDiv center={[42.14, 24.78]} zoom={9} scrollWheelZoom={true}>
       <TileLayer
