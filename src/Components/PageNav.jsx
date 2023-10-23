7f477b09d32368aab50d7d717a2cd6440b12acc0
Oktay Rasimov <oktayrasimov123@gmail.com>
2023-10-23 23:45:21 +0300

    made the navbar its own component and made it reusable

--- src/Components/PageNav.jsx
+++ src/Components/PageNav.jsx
@@ -1,26 +1,12 @@
 import Logo from "./Logo";
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
 import { HiMenu } from "react-icons/hi";
+import PageNavUlComponent from "./PageNavUl";
 
 const PageNavContainer = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`;
-
-const PageNavUl = styled.ul`
-  display: flex;
-  gap: 3.6rem;
-  list-style-type: none;
-
-  li {
-    color: var(--color-white-100);
-  }
-
-  @media (width<=750px) {
-    display: none;
-  }
 `;
 
 const PageNavMobile = styled.div`
@@ -32,33 +18,13 @@ const PageNavMobile = styled.div`
   }
 `;
 
-const PageNavStyledLink = styled(NavLink)`
-  font-size: 2.4rem;
-  padding: 1rem 2rem;
-  &.active {
-    /* padding: 1rem 2rem; */
-
-    border-radius: 10px;
-    background-color: var(--color-green-100);
-  }
-`;
-
 function PageNav() {
   return (
     <PageNavContainer>
       <Logo />
 
-      <PageNavUl>
-        <li>
-          <PageNavStyledLink to="/pricing">Pricing</PageNavStyledLink>
-        </li>
-        <li>
-          <PageNavStyledLink to="/product">Product</PageNavStyledLink>
-        </li>
-        <li>
-          <PageNavStyledLink to="/login">Login</PageNavStyledLink>
-        </li>
-      </PageNavUl>
+      <PageNavUlComponent />
+
       <PageNavMobile>
         <HiMenu />
       </PageNavMobile>
---
+++ src/Components/PageNavUl.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+const PageNavUl = styled.ul`
+  display: flex;
+  gap: 3.6rem;
+  list-style-type: none;
+
+  li {
+    color: var(--color-white-100);
+  }
+
+  @media (width<=750px) {
+    display: none;
+  }
+`;
+
+const PageNavStyledLink = styled(NavLink)`
+  font-size: 2.4rem;
+  padding: 1rem 2rem;
+  &.active {
+    /* padding: 1rem 2rem; */
+
+    border-radius: 10px;
+    background-color: var(--color-green-100);
+  }
+`;
+
+function PageNavUlComponent() {
+  return (
+    <PageNavUl>
+      <li>
+        <PageNavStyledLink to="/pricing">Pricing</PageNavStyledLink>
+      </li>
+      <li>
+        <PageNavStyledLink to="/product">Product</PageNavStyledLink>
+      </li>
+      <li>
+        <PageNavStyledLink to="/login">Login</PageNavStyledLink>
+      </li>
+    </PageNavUl>
+  );
+}
+
+export default PageNavUlComponent;
